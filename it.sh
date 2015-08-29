8ada90da52a36c405888c27f10b5af1a0b06e163
Andesson <awesousa@gmail.com>
2015-08-29 17:38:34 -0300

    aasousa@xubuntu:site_maintenance

--- it.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-cd ~/Documentos/GitHub
-echo -n '[sync] github '
-date
-git pull origin master
-git add .
-git commit -m date
-git push origin master
--- teste.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-	system("clear");
-	printf("git:~docs/github$ ");
-	system("read COMAND echo $COMAND");
-}
